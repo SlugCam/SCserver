723c6ae53c595aa5c81f167691309be95e363440
Leland Miller <lelandmiller@gmail.com>
2014-07-15 19:33:58 -0700

    Added app to repo.

---
+++ app/app/js/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+var baseUrl = 'http://localhost:7891/';
+
+/* Controllers */
+
+
+angular.module('myApp.controllers', [])
+    .controller('MyCtrl1', ['$scope', '$http',
+        function($scope, $http) {
+            $http.get(baseUrl + 'dump').success(function(data) {
+                $scope.messages = data;
+            });
+
+
+
+        }
+    ])
+    .controller('MyCtrl2', ['$scope',
+        function($scope) {
+
+        }
+    ]);
