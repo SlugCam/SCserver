279017df5772d48454a306377a9b3156ce1fcbe2
Leland Miller <lelandmiller@gmail.com>
2014-07-15 19:34:32 -0700

    Changed db port

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var dataDir = '_data';
 module.exports = {
     "db": {
-        "port": 7890,
+        "port": 7880,
         "hostname": "localhost",
         "path": path.join(__dirname, dataDir, 'db')
     },
