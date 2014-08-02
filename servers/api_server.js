ac80aa524284bf3fa68a026b6023d3ea8f7dcfa2
Leland Miller <lelandmiller@gmail.com>
2014-08-02 10:01:42 -0700

    Switched to express for api server.

--- servers/api_server.js
+++ servers/api_server.js
@@ -4,11 +4,41 @@
 // Contains the server for the external facing http server that application
 // programmers can interface with.
 
-var http = require('http');
+//var http = require('http');
 var url = require('url');
 var db = require('./lib/db');
 var log = null;
 
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    var uri = url.parse(req.url).pathname;
+    log.info('request for ' + uri);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
+app.get('/dump', function(req, res) {
+    db.getMessages(function (data) {
+        res.json(data);
+    });
+    //res.send('hello world');
+});
+
+// Starts the server on the port number of the argument
+exports.listen = function(port, logger) {
+    if (!logger) {
+        console.error('api_server: requires a log object');
+        return;
+    }
+    log = logger;
+    log.info('server bound to port ' + port);
+    app.listen(port);
+};
+/*
 routes = {};
 
 var server = http.createServer(function(req, res) {
@@ -48,3 +78,4 @@ exports.listen = function(port, logger) {
         log.info('server bound to port ' + port);
     });
 };
+*/
