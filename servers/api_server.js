034a38e92e414de05643e6eb96177385bfa9249a
Leland Miller <lelandmiller@gmail.com>
2014-08-24 09:02:45 -0700

    Adding camera setting route to API server.

--- servers/api_server.js
+++ servers/api_server.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
+
 });
 
 app.use(bodyParser.json({type:'*/json'}));
